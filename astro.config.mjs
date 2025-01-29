f4226b4f71434773a90e61ce2391e1213b81aec6
Sergio <sergioaltable@gmail.com>
2025-01-29 08:35:23 +0100

    Fix: Configurar base para GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
+  base: "/porfolio2/",
   viewTransitions: true // Enable view transitions
 });
